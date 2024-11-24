2b580072151cf3536fa7e332367e4731c972514a
ShaunAUS <wnsqud70@gmail.com>
2024-11-24 14:22:34 +0900

    지연 코루틴

---
+++ src/main/kotlin/section6/code1/Code6-1.kt
@@ -0,0 +1,17 @@
+package section6.code1
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking<Unit> {
+    val lazyJob = launch(start = CoroutineStart.LAZY) {
+        println(" [${Thread.currentThread().name}] 작업1 실행")
+    }
+    lazyJob.start()
+}
+
+
