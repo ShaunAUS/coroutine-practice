627c91507494db51eed098130dafd7c73680c0cc
ShaunAUS <wnsqud70@gmail.com>
2024-11-26 14:29:13 +0900

    supervisorJob + coroutineScope 을 이용한 예외전파 차단

---
+++ src/main/kotlin/section15/code1/Code15-3.kt
@@ -0,0 +1,28 @@
+package section15.code1
+
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main(): Unit = runBlocking {
+    val coroutineScope = CoroutineScope(SupervisorJob())
+    coroutineScope.apply {
+        launch(CoroutineName("Coroutine1")) {
+            launch(CoroutineName("Coroutine3")) {
+                throw Exception("예외 발생 꺅")
+            }
+            delay(100L)
+            println("${Thread.currentThread().name} 코루틴 실행")
+        }
+        launch(CoroutineName("Coroutine2")) {
+            delay(100L)
+            println("${Thread.currentThread().name} 코루틴 실행")
+        }
+    }
+    delay(1000L)
+}
